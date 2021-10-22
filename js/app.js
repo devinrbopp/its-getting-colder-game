2e16660d5c66dd794bc9272db1746b87db3b47ba
devinrbopp <devinrbopp@gmail.com>
2021-10-22 12:35:50 -0600

    shelter counter working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+let shelters = 0
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('dom content loaded')
+    buildShelter.addEventListener('click', () => {
+        shelters++
+        if (shelters == 1) {
+            const shelterDiv = document.createElement('div')
+            shelterDiv.innerText = `shelters: ${shelters}`
+            shelterCounter.appendChild(shelterDiv)
+        } else {
+            shelterCounter.removeChild(shelterCounter.childNodes[0])
+            const shelterDiv = document.createElement('div')
+            shelterDiv.innerText = `shelters: ${shelters}`
+            console.log('shelters:', shelters)
+            shelterCounter.appendChild(shelterDiv)
+        }
+    })
 })

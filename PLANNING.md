a0ea97d3a25a0524f4a18d6d1aca642830f1f44c
devinrbopp <devinrbopp@gmail.com>
2021-10-22 14:24:43 -0600

    timer added

---
+++ PLANNING.md
@@ -0,0 +1,7 @@
+# Basic flow for player
+* page loads, displays title, instructions, and a single button to build a shelter
+* on click, the page changes. title disappears, instructions disappear, counters appear at the bottom
+* once you've built 5 shelters, display the message 'you've done well, a place to hide from the rain'
+* create a population incrementer that adds one person per farm plot per minute
+* create a timer that progresses 1%/15sec
+* default end condition can be do you have 10 people
--- js/app.js
+++ js/app.js
@@ -1,22 +1,36 @@
+let timer = 0
 let shelters = 0
 let farmPlots = 0
 
+// display a message when 5 shelters are built
+const shelterTutorial = () => {
+    if (shelters == 5) {
+        console.log('5 shelters built')
+        startTimer()
+    }
+}
+
+const startTimer = () => {
+    console.log('timer started')
+    setInterval(()=>{
+        console.log(timer)
+        timer++
+    },1000)
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('dom content loaded')
+    // build shelter event listener
+    window.addEventListener('click', () => {
+        shelterTutorial()
+    })
     buildShelter.addEventListener('click', () => {
         shelters++
-        if (shelters == 1) {
-            shelterNum.innerText = shelters
-        } else {
-            shelterNum.innerText = shelters
-        }
-    })
+        shelterNum.innerText = shelters
+        })
+    // build farm plot event listener
     buildFarmPlot.addEventListener('click', () => {
         farmPlots++
-        if (farmPlots == 1) {
-            farmPlotNum.innerText = farmPlots
-        } else {
-            farmPlotNum.innerText = farmPlots
-        }
-    })
+        farmPlotNum.innerText = farmPlots
+        })
 })

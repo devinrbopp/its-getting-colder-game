ffcf7048caf766151c9282f0668db3d1135cf00c
devinrbopp <devinrbopp@gmail.com>
2021-10-23 20:48:45 -0600

    further updates to builds array

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
 // global variables
 let timer = 1
 let season = 'spring'
-let shelters = 0
-let farmPlots = 0
-let food = 0
 
-const builds = {
+const build = {
+    food: {
+        count: 0,
+        cost: 0,
+        winPoints: 0
+    },
     shelter: {
+        count: 0,
         cost: 10,
         winPoints: 1
     },
     farmPlot: {
+        count: 0,
         cost: 10,
         winPoints: 2
     }
 }
 
+
 // display a message when 5 shelters are built
 const shelterTutorial = () => {
-    if (shelters == 5) {
+    if (build.shelter.count == 5) {
         console.log('5 shelters built')
         startTimer()
         // this will need to change later--don't want to use removeEventListener
@@ -28,7 +33,7 @@ const shelterTutorial = () => {
 
 // simple win check placeholder
 const winCheck = () => {
-    if (food > 10 && shelters > 10) {
+    if (build.food.count > 10 && build.shelter.count > 10) {
         console.log('you win')
     } else {
         console.log('you lose')
@@ -37,15 +42,15 @@ const winCheck = () => {
 
 // intervals for initiation later
 const foodInterval = setInterval(()=> {
-        console.log(farmPlots)
-        food += (farmPlots * .002)
-        console.log(food)
-        foodNum.innerText = `${Math.floor(food)}`
+        // console.log(farmPlots)
+        build.food.count += (build.farmPlot.count * .002)
+        // console.log(food)
+        foodNum.innerText = `${Math.floor(build.food.count)}`
     }, 10)
 
 // startTimer kicks off the interval function that cycles through the seasons
 const startTimer = () => {
-    console.log('timer started')
+    // console.log('timer started')
     seasonDisplay.innerText = `${season}, ${Math.floor(timer)}%`
     const timerMechanism = setInterval(()=>{
         if (timer < 75) {
@@ -54,19 +59,19 @@ const startTimer = () => {
             timer += 1.5
         }
         seasonDisplay.innerText = `${season}, ${Math.floor(timer)}%`
-        console.log(timer)
+        // console.log(timer)
         if (timer < 25) {
-            console.log('spring')
+            // console.log('spring')
             season = 'spring'
         } else if (timer >= 25 && timer < 50) {
-            console.log('summer')
+            // console.log('summer')
             season = 'summer'
         } else if (timer >= 50 && timer < 75) {
-            console.log('fall')
+            // console.log('fall')
             season = 'fall'
         } else if (timer >= 75 && timer < 100) {
-            console.log('it\'s getting colder....')
-            season = 'it\'s getting colder'
+            // console.log('it\'s getting colder....')
+            season = 'it\'s getting colder.....'
         } else if (timer >= 100) {
             console.log('game over')
             seasonDisplay.innerText = 'winter is here.'
@@ -82,23 +87,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // build shelter event listener
     window.addEventListener('click',shelterTutorial)
     gatherFood.addEventListener('click', () => {
-        food++
+        build.food.count++
     })
     buildShelter.addEventListener('click', () => {
-        if (food >= builds.shelter.cost) {
-            shelters++
-            shelterNum.innerText = shelters
-            food-=builds.shelter.cost
-        } else {
-            console.log('you need more food to do that')
+        if (build.food.count >= build.shelter.cost) {
+            build.shelter.count++
+            shelterNum.innerText = build.shelter.count
+            build.food.count-=build.shelter.cost
         }
     })
     // build farm plot event listener
     buildFarmPlot.addEventListener('click', () => {
-        if (food >= builds.farmPlot.cost) {
-            farmPlots++
-            farmPlotNum.innerText = farmPlots
-            food-=builds.farmPlot.cost
+        if (build.food.count >= build.farmPlot.cost) {
+            build.farmPlot.count++
+            farmPlotNum.innerText = build.farmPlot.count
+            build.food.count-=build.farmPlot.cost
         }
     })
 })
